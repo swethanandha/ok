d500052dff35f1e1b07ebd5a7e8e3ffae4cb5f8d
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-15 16:25:36 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int num,np,rev;
+	cin>>num;
+             if(num!=0)
+	{
+		np=num%10;
+		revnum=num*10+np;
+		
+	}
+	if(revnum=num)
+             
+             {
+             	cout<<num<<"palindrome";
+             }
+	else
+	{
+		cout<<num<<"not palindrome";
+	}
+	else
+	{
+		
+	}
+	return 0;
+}
