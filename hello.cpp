d611d7a2e0ee9e11be68b75ab1d82a8ddb862ccc
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-15 11:55:41 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num,i;
+    cout<<"number of times"<<endl;
+    cin>>num;
+    cout<<" " <<num<<endl;
+    for(i=0;i<num;i++)
+    {
+    cout<<"Hello"<<endl;	
+    }
+}
