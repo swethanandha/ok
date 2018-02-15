c911ebd9c5761dcf7f4c643681f4d4b027a72f8e
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-15 09:29:54 +0530

    Create voewl.cpp

---
+++ voewl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	cout<<"enter the character "<<endl;
+	char fir;
+	cin>>fir;
+	cout<< " "<<fir<<" ";
+  char a[20]={'A','E','I','O','U','a','e','i','o','u'},i;
+  if(fir==a[i])
+  {
+        cout<<"vowel";
+   }
+            
+  else
+  {
+        cout<<"consonant";
+  }
+            
+}
