2e2ee5cac1c92d7f2cd26fb6ee01c789f784bf7a
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-22 17:03:04 +0530

    Create amstrong.cpp

---
+++ amstrong.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int number;
+    int remain;
+    int result=0;
+    int y;
+    
+
+    cout<<"Enter a three digit integer: ";
+    cin>>number;
+
+    y = number;
+
+    while (y != 0)
+    {
+        remain = y%10;
+        result =result+ remain*remain*remain;
+        y=y/10;
+    }
+
+    if(result == number)
+      cout<<number<<"amstrong number";
+    else
+        cout<<number<<"not an amstrong number";
+
+    return 0;
+}
