81e128dcd22c6959dc1ba13072da5f9c788c81a7
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-28 17:52:21 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a[10];
+	int i,temp;
+	 for(i=0;i<=10;i++)
+	 {
+	 	cin>>a[i];
+		
+		
+	 }
+	
+
+	for(i=1;i<10;i++)
+	{
+		if(a[0]<a[i])
+		{
+			a[0]=a[i];
+		}
+		
+	}
+	cout<<"maximum element"<<a[0];
+	return 0;
+}
