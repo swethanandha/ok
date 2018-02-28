b689ec40381309eb785fc9301c6c59e1946bb6cd
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-02-28 17:24:40 +0530

    Create 40.cpp

---
+++ 40.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+
+    cout << "Enter a positive number: ";
+    cin >> n;
+    cout<<n<<endl;
+    
+    cout<<t1<<",";
+    cout<<t2<<",";
+
+    nextTerm = t1 + t2;
+
+    while(nextTerm <= n)
+    {
+        cout << nextTerm << ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+    return 0;
+}
