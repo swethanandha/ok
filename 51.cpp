5dd06427988724d077178663534fd789a6cc4068
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-02 10:28:45 +0530

    Create 51.cpp

---
+++ 51.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string s;
+	cin>>s;
+	cout<<"string :"<<s<<endl;
+	int k,i;
+	cin>>k;
+	cout<<"number of times :"<<k<<endl;
+	for(i=1;i<=k;i++)
+	{
+		cout<<s<<endl;
+	}
+	
+	return 0;
+}
