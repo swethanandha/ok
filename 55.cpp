275a527cb982b21be0f01f6b73d94de80ebffca1
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-05 18:01:01 +0530

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main( )
+{
+	int n1;
+	int n2;
+	cin>>n1;
+	cout<<"number1 :"<<n1<<endl;
+	cin>>n2;
+	cout<<"number2 :"<<n2<<endl;
+	int n3;
+	n3=n1+n2;
+	cout<<n3;
+	if(n3%2==0)
+	{
+	    cout<<"even";
+	}
+	even
+	{
+	    cout<<"odd";
+	}
+}
