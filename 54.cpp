13000c9a572c1dfe66b4363478a02cdea6ef4d6c
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-06 17:26:03 +0530

    Create 54.cpp

---
+++ 54.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n,num;
+ cout<<"Enter the number :";
+ cin>>n;
+ num=n+1;
+ cout<<n <<" next nuber is"<<num<<endl;
+ if(num%2==0)
+ {
+     cout<<num<<" is even";
+ }
+ else
+ {
+     cout<<num<<" is odd";
+ }
+
+    return 0;
+}
