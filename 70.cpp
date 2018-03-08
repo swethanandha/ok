d94838df061ced5f0145928f9f48d7960671f5a0
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-08 16:57:29 +0530

    Create 70.cpp

---
+++ 70.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+ 
+int main()
+{ 
+    int n1,n2,n;
+    cout<<"Enter the first number :";
+    cin>>n1;
+    n2 = n1/2;
+    n2=n2*4;
+    //n = n2*2
+    cout<<n2;
+}
