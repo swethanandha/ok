74dc800a2adaf991bb3f5184fb8e0b10522b9e94
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-08 17:12:58 +0530

    Create 49.cpp

---
+++ 49.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,n2,i;
+	cout<<"enter the first number :";
+	cin>>n;
+	cout<<"enter the second number :";
+    cin>>n2;
+    i=n+n2;
+    cout<<"sum of numbers is :"<<i;
+}
