d1c2c2fb7847377afffc2ede023fec1191a98a43
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-08 17:01:39 +0530

    Create 66.cpp

---
+++ 66.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int number,i;
+	cin>>number;
+	if(num<=1000)
+	  {
+	for(i=2;i<number;i++)
+	{
+	if(number%i==0)
+	{
+		cout<<number<<" notprime";
+		break;
+	}
+	else
+	{
+		cout<< number<<" prime number";
+		break;
+	}
+	}
+	
+	}
+	else{
