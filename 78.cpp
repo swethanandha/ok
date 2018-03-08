b295ea267dba10692efd8c009f2df39240171e7b
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-08 17:46:15 +0530

    Create 78.cpp

---
+++ 78.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   int n;
+   int res;
+   cout<<"enter the number :";
+   cin>>n;
+   if(n%13==0)
+   {
+       cout<<"yes,multiple of 13";
+   }
+   else
+   {
+       cout<<"no,not multiple of 13";
+   }
+}
