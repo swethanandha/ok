fa3ce30800c9d0fe5b913fbfef6ba176ec61295e
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-08 17:40:07 +0530

    Create 74.cpp

---
+++ 74.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   double n;
+   int res;
+   cout<<"enter the number to check";
+   cin>>n;
+   res=n+1;
+   cout<<res;
+  
+}
