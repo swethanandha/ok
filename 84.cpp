e373ee9e423a783f9b3a619fd1fcdbe738b52fe9
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-15 16:48:49 +0530

    Create 84.cpp

---
+++ 84.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter the character :";
+    cin>>a;
+    cout<<"ascii value of charcter :"<<int(a);
+    return 0;
+}
