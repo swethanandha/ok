6d6214ac23da8abf7d6d3a04e1df2944efbfe547
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-20 10:32:30 +0530

    Create 93.cpp

---
+++ 93.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float cel;
+  float kel=273;
+  cout<<"enter the temparature in celsius :";
+  cin>>cel;
+  kel=cel+kel;
+  cout<<"temparature in kelvin :"<<kel;
+    
+}
