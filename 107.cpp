37d42bcc6f9e8e077e57a7af6cb96705db9c9ba2
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-20 17:57:58 +0530

    Create 107.cpp

---
+++ 107.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int n,n1,n2,res;
+cout<<"enter the numbers ";
+cin>>n>>n1>>n2;
+res=(n*n1)/n2;
+cout<<"result:"<<res;
+
+}
