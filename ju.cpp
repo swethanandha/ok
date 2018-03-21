02edc48f6e93c100a525e3492063058cef4791a4
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-21 17:01:16 +0530

    Create ju.cpp

---
+++ ju.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<n/2;
+    }
+    else
+    {
+        cout<<n;
+    }
+}
