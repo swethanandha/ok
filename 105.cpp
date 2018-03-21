69a084d49534c2fd870ef7a17afe0a09769c3925
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-21 18:03:43 +0530

    Create 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+ char a;
+ int i;
+ cin>>a;
+ for(i=(int)a-96;i<=26;i++)
+
+    {
+    if((int)a-96==i)
+    {
+        cout<<i;
+    }
+    }
+}
