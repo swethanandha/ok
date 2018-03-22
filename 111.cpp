a060cda74296935b6969fc89cb138d41b54b950c
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-03-22 16:48:11 +0530

    Create 111.cpp

---
+++ 111.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+ 
+ int n,count=0;
+ cout<<"Enter the number :";
+ cin>>n;
+ while(n!=0)
+ {   
+     n=n/10;
+     count=count+1;
+     
+ }
+    cout<<"number of digits:"<<count;
+}
