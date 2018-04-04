869d4e4cd72a592a31d625c40d39d319cd489223
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-04-04 13:42:53 +0530

    Create play4.cpp

---
+++ play4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   string str,str1;
+   cout<<"Enter the string :";
+   cin>>str;
+   str.append(1,'.');
+   cout<<"output:"<<str;
+
+    return 0;
+}
