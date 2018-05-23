65366e783414f81b2d85a4030c82590493e76e8d
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-05-23 12:33:24 +0530

    Create play55.cpp

---
+++ play55.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+  int main()
+{
+    
+string s,s1;
+cin>>s;
+cin>>s1;
+
+
+if(s==s1)
+{
+    cout<<"yes ,strings are same";
+}
+else
+{
+    cout<<"no,strings are not same";
+    
+}
+}
