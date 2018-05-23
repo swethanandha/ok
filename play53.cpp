0396f4263305f3b9d06cd479893cfd5db77e17a6
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-05-23 12:15:54 +0530

    Create play53.cpp

---
+++ play53.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+  
+
+int main()
+{
+    
+   
+	
+string s;
+cin>>s;
+int i,count=0;
+for(i=0;s[i]!='\0';i++)
+{
+    count++;
+}
+cout<<"length of string ;"<<count;
+}
