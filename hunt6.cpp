64a08c9bb3551601c81fb6e2de50aaed26b92de3
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-05-24 15:38:18 +0530

    Create hunt6.cpp

---
+++ hunt6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[34],n,i,j,b[56];
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    for(i=0;i<n;i++)
+    { 
+       int count=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+               count=count+1;
+            }
+        }
+        if(count==0)
+        {
+            cout<<a[i];
+            break;
+        }
+    }
+    
+    
+}
