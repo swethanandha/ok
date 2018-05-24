f145675c9252e3f0e18a174eb81e232064a50305
swethanandha <36472949+swethanandha@users.noreply.github.com>
2018-05-24 11:49:33 +0530

    Create hunt1.cpp

---
+++ hunt1.cpp
@@ -0,0 +1,19 @@
+for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {     
+            if(a[i]>a[j])
+        {                temp=a[i];
+                         a[i]=a[j];
+                         a[j]=temp;
+        }
+    
+         
+        }
+       
+          
+    
+    }
+    
+    
+          }
